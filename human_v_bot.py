f269ff4401841e3f133a29de2badded698f77283
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-10 22:43:47 +0900

    fix: apply SuggestionAgent to human_v_bot

--- human_v_bot.py
+++ human_v_bot.py
@@ -3,12 +3,14 @@ from connect5 import agent
 from connect5 import board as connect5_board
 from connect5 import types
 from connect5.utils import print_board, print_move, point_from_coords
+from connect5.agent.presuggestion import presuggestion
+from connect5.agent.SuggestionAgent import SuggestionAgent
 from six.moves import input
 
 def main():
     board_size = 8
     game = connect5_board.GameState.new_game(board_size)
-    bot = agent.RandomBot()
+    bot = SuggestionAgent(1000, 1.3, presuggestion)
 
     while not game.is_over():
         # print(chr(27) + "[2J")
