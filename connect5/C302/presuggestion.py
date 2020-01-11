24329864edf303d9eb161be35266328e734d1cbc
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-12 04:45:14 +0900

    feat: 대각선 방향에 돌 세 개가 있나 체크

--- connect5/C302/presuggestion.py
+++ connect5/C302/presuggestion.py
@@ -59,6 +59,46 @@ def presuggestion(game_state, moves):
             return None
         return (left, left_color)
 
+    # 오른쪽 위로 대각선
+    def get_diagonal_top_right_of_point(point):
+        if (point.row + 1 >= num_rows):
+            return None
+        if (point.col + 1 >= num_cols):
+            return None
+        diagonal = Point(point.row + 1, point.col + 1)
+        diagonal_color = get_color(diagonal)
+        if diagonal_color == 0:
+            return None
+        return (diagonal, diagonal_color)
+
+    # 오른쪽 아래로 대각선
+    def get_diagonal_bottom_right_of_point(point):
+        if (point.col + 1 >= num_cols):
+            return None
+        diagonal = Point(point.row - 1, point.col + 1)
+        diagonal_color = get_color(diagonal)
+        if diagonal_color == 0:
+            return None
+        return (diagonal, diagonal_color)
+
+    # 왼쪽 위로 대각선
+    def get_diagonal_top_left_of_point(point):
+        if (point.row + 1 >= num_rows):
+            return None
+        diagonal = Point(point.row + 1, point.col - 1)
+        diagonal_color = get_color(diagonal)
+        if diagonal_color == 0:
+            return None
+        return (diagonal, diagonal_color)
+
+    # 왼쪽 아래로 대각선
+    def get_diagonal_bottom_left_of_point(point):
+        diagonal = Point(point.row - 1, point.col - 1)
+        diagonal_color = get_color(diagonal)
+        if diagonal_color == 0:
+            return None
+        return (diagonal, diagonal_color)
+
     def check_abstract(point, getter):
         match_color = None
         is_exist = None
@@ -75,37 +115,41 @@ def presuggestion(game_state, moves):
                 return False
         return True
 
-    # 위에 같은 색 돌 세 개가 있나 체크
-    def check_three_in_top(point):
-        return check_abstract(point, get_top_of_point)
-
-    # 아래에 같은 색 돌 세 개가 있나 체크
-    def check_three_in_bottom(point):
-        return check_abstract(point, get_bottom_of_point)
-
-    # 오른쪽에 같은 색 돌 세 개가 있나 체크
-    def check_three_in_left(point):
-        return check_abstract(point, get_left_of_point)
-
-    # 왼쪽에 같은 색 돌 세 개가 있나 체크
-    def check_three_in_right(point):
-        return check_abstract(point, get_right_of_point)
-
     # iterate all moveable moves
     print([m.point for m in moves])
     for move in moves:
         current_point = move.point
 
-        if check_three_in_top(current_point):
+        # 위에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_top_of_point):
             print(f'{current_point} 위에 돌 세 개가 있습니다.')
 
-        if check_three_in_bottom(current_point):
+        # 아래에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_bottom_of_point):
             print(f'{current_point} 아래에 돌 세 개가 있습니다.')
 
-        if check_three_in_right(current_point):
+        # 오른쪽에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_left_of_point):
             print(f'{current_point} 오른쪽에 돌 세 개가 있습니다.')
 
-        if check_three_in_left(current_point):
+        # 왼쪽에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_right_of_point):
             print(f'{current_point} 왼쪽에 돌 세 개가 있습니다.')
+
+        # 대각선 오른쪽 위에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_diagonal_top_right_of_point):
+            print(f'{current_point} 대각선 오른쪽 위에 돌 세 개가 있습니다.')
+
+        # 대각선 오른쪽 아래에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_diagonal_bottom_right_of_point):
+            print(f'{current_point} 대각선 오른쪽 아래에 돌 세 개가 있습니다.')
+
+        # 대각선 왼쪽 위에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_diagonal_top_left_of_point):
+            print(f'{current_point} 대각선 왼쪽 위에 돌 세 개가 있습니다.')
+
+        # 대각선 완쪽 아래에 같은 색 돌 세 개가 있나 체크
+        if check_abstract(current_point, get_diagonal_bottom_left_of_point):
+            print(f'{current_point} 대각선 왼쪽 아래에 돌 세 개가 있습니다.')
 
     return None
